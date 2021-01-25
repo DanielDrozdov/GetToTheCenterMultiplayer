0360744ccdacaee26d5308549108cb262f2f8229
Danil Drozdov <danilka.drozdov2004@gmail.com>
2021-01-25 19:14:32 +0200

    Add a project

---
+++ Assets/Scripts/CameraFollowingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowingController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 playerCameraOffset = new Vector3(0,2,4);
+    private float _lerpSpeed = 0.9f;
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + playerCameraOffset, _lerpSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TapPlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapPlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rb.velocity = new Vector3(0, 0, -0.5f);
+        }
+    }
+}
